bf5f7f1dc5ff588525dfe0b3d7c748f3eb47018c
J-Lindvig <54498188+J-Lindvig@users.noreply.github.com>
2022-02-20 23:31:13 +0100

    Update gasprices_dk.py

--- custom_components/gasprices_dk/gasprices_dk.py
+++ custom_components/gasprices_dk/gasprices_dk.py
@@ -25,10 +25,3 @@ class gasprices_dk_api:
 	def getGasPrices(self):
 		for company in GAS_COMPANIES.keys():
 			self._gasPrices[company] = getattr(self._parser, company)(GAS_COMPANIES[company])
-
-	def _getDiscount(self):
-		for company in GAS_COMPANIES.keys():
-			if 'discount' in GAS_COMPANIES[company].keys():
-				print('Before: ' + str(self._gasPrices[company][GAS_95]['price']))
-				self._gasPrices[company][GAS_95]['price'] = float("{:.2f}".format(self._gasPrices[company][GAS_95]['price'] - GAS_COMPANIES[company]['discount']))
-				print('After: ' + str(self._gasPrices[company][GAS_95]['price']))
